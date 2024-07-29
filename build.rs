44da3455cae2232634112354d7a3efc3a03c57fe
lach-g <lachlan.greenland@gmail.com>
2024-07-29 18:11:49 +0800

    feat: add sparkplug-b proto file and build rust bindings via cargo

---
+++ build.rs
@@ -0,0 +1,26 @@
+use protobuf_codegen::Customize;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::{env, fs};
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("protos");
+    let proto_path = Path::new("./protos");
+    fs::create_dir_all(&dest_path).unwrap();
+
+    // Run protoc
+    protobuf_codegen::Codegen::new()
+        .pure()
+        .out_dir(&dest_path.to_str().unwrap())
+        .inputs(&[proto_path.join("sparkplug_b.proto").to_str().unwrap()])
+        .includes(&[proto_path.to_str().unwrap()])
+        .customize(Customize::default())
+        .run()
+        .expect("Protoc Error");
+
+    // Create mod.rs accordingly
+    let mut mod_file = File::create(dest_path.join("mod.rs")).unwrap();
+    writeln!(mod_file, "pub mod sparkplug_b;").unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub use protobuf;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+pub use sparkplug_b::*;
